bde9564cc6bf856dba311cdb04928de55b6c4aec
TALHA KOSEN <talha.kosen@turkcell.com.tr>
2016-11-01 19:54:02 +0200

    extract redundancy and prepare for push

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,11 @@
 class ArticlesController < ApplicationController
-
+    before_action :set_article, only: [:edit,:update,:destroy,:show]
     def new
         @article = Article.new
-
     end
 
     def edit
-      @article = Article.find(params[:id])
+
     end
 
     def create
@@ -23,7 +22,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-      @article = Article.find(params[:id])
+
       if @article.update(article_params)
         flash[:notice] = "Article was succesfully updated"
         redirect_to article_path(@article)
@@ -33,7 +32,7 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
+
     end
 
     def index
@@ -41,13 +40,17 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-      @article = Article.find(params[:id])
+
       @article.destroy
       flash[:notice] = "Article was succesfully deleted"
       redirect_to articles_path
     end
 
     private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
         params.require(:article).permit(:title,:description)
     end
