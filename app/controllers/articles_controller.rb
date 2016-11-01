b299d963e4341629d111a43ea8a68996e909ff40
TALHA KOSEN <talha.kosen@turkcell.com.tr>
2016-11-01 22:02:17 +0200

    add style message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,57 +1,58 @@
 class ArticlesController < ApplicationController
-    before_action :set_article, only: [:edit,:update,:destroy,:show]
-    def new
-        @article = Article.new
+  before_action :set_article, only: [:edit, :update, :destroy, :show]
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    # @article = Article.new(article_params)
+    # @article.save
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = "Article was succesfully created"
+      redirect_to article_path(@article)
+    else
+      # render :new
+      render 'new'
     end
+  end
 
-    def edit
+  def update
 
+    if @article.update(article_params)
+      flash[:success] = "Article was succesfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
     end
+  end
 
-    def create
-         # @article = Article.new(article_params)
-         # @article.save
-      @article = Article.new(article_params)
-      if @article.save
-        flash[:notice] = "Article was succesfully created"
-        redirect_to article_path(@article)
-      else
-        # render :new
-        render 'new'
-      end
-    end
+  def show
 
-    def update
+  end
 
-      if @article.update(article_params)
-        flash[:notice] = "Article was succesfully updated"
-        redirect_to article_path(@article)
-      else
-        render 'edit'
-      end
-    end
+  def index
+    @articles = Article.all
+  end
 
-    def show
+  def destroy
 
-    end
+    @article.destroy
+    flash[:danger] = "Article was succesfully deleted"
+    redirect_to articles_path
+  end
 
-    def index
-      @articles = Article.all
-    end
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    def destroy
-
-      @article.destroy
-      flash[:notice] = "Article was succesfully deleted"
-      redirect_to articles_path
-    end
-
-    private
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
-    def article_params
-        params.require(:article).permit(:title,:description)
-    end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
