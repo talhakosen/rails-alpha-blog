c5d6d0764704508661addd4851b978fbae81f7e2
TALHA KOSEN <talha.kosen@turkcell.com.tr>
2016-11-01 11:20:36 +0200

    add error message , add show

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'pages#home'
+  get 'about', to:'pages#about'
+
+  resources :articles
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
-  root 'pages#home'
-  get 'about', to:'pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
