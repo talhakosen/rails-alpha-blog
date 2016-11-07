eee8f51fa5292e8b831e690ba7db4c600f3ee042
TALHA KOSEN <talha.kosen@turkcell.com.tr>
2016-11-07 14:27:03 +0200

    test cases wrote

--- Gemfile
+++ Gemfile
@@ -37,7 +37,7 @@ gem 'will_paginate', '~> 3.1', '>= 3.1.5'
 
 gem 'bootstrap-will_paginate', '~> 0.0.10'
 
-group :development, :test do
+group :development do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
 
---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+class ListCategoriesTest < ActionDispatch::IntegrationTest
+  def setup
+    @category = Category.create(name: "books")
+    @category_2 = Category.create(name: "musics")
+  end
+
+  test "should show category listing" do
+    get categories_path
+    assert_template 'categories/index'
+
+    assert_select "a[href=?]",category_path(@category), text: @category.name
+    #assert_select "a[href=?]",category_path[@category_2], text:@category_2.name
+  end
+end
